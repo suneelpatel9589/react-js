cff97e9bd86a995f82549b9dae7999854fcc4ac8
suneel patel <suneelpatel9589@gmail.com>
2025-11-10 11:07:47 -0800

    d

---
+++ firstpro/src/App.jsx
@@ -0,0 +1,13 @@
+import Ch1 from "./Ch1"
+
+function App(){
+  return(
+    <>
+    <Ch1/>
+    
+    </>
+  )
+  
+}
+
+export default App
---
+++ firstpro/src/Ch1.jsx
@@ -0,0 +1,12 @@
+
+function Ch1(){
+    return(
+        <>
+        
+        <h1>suneel patel</h1>
+        
+        </>
+    )
+}
+
+export default Ch1
